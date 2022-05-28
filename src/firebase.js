5cc9523bb35af25c9c8776eabf177f0aa691abe4
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2022-05-28 16:38:24 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDftw9h4n5HRJep8HS5Mrby3ggY5zDY02A",
+  authDomain: "company-website-3d1d7.firebaseapp.com",
+  projectId: "company-website-3d1d7",
+  storageBucket: "company-website-3d1d7.appspot.com",
+  messagingSenderId: "845038023734",
+  appId: "1:845038023734:web:470c27c0d9f4c79c845497",
+  measurementId: "G-RP9EMSJEPJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
