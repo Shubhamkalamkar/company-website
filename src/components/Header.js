28e6f682cd6db3ac5c416024c7b8b893a63b7487
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2023-04-27 22:44:09 +0530

    profile img add

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,36 @@ import React, { useEffect, useState } from "react";
 import "./header.css";
 
 import { useUserAuth } from "../context/UserAuthContext";
+import { updateProfile } from "firebase/auth";
+
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth, storage } from "../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 
 export const Header = () => {
   const { logOut, user } = useUserAuth();
   const [isactive, setIsactive] = useState(false);
-
+  
   const [userName, setUserName] = useState("");
+
+  const [userdata, setUserdata] = useState();
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const [url, setUrl] = useState(null);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       // console.log(user);
       if (user) {
         setUserName(user.displayName);
+        setUserdata(user)
       } else {
         setUserName("");
       }
     });
   }, []);
+
+  const storageRef = ref(storage, "profilePhotos");
 
   const navigate = useNavigate();
   const handleLogout = async () => {
@@ -35,6 +47,42 @@ export const Header = () => {
     setIsactive(!isactive);
   };
 
+  const uploadImg = ()=>{
+    
+    uploadBytes(storageRef, profilePhoto).then(()=>{
+      getDownloadURL(storageRef).then((url)=>{
+        setUrl(url);
+        console.log(url)
+      }).catch(error =>{
+        console.log(error.message)
+      });
+      setProfilePhoto(null)
+    }).catch((e)=>{
+      console.log(e.message)
+    });
+
+    // uploadTask.on(
+    //   (error) => {
+    //     // Handle unsuccessful uploads
+    //     console.log("error while uploading img")
+        
+    //   },
+    //   () => {
+    //     getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+    //       console.log("File available at", downloadURL);
+    //       await updateProfile(userdata,{
+    //         photoURL:downloadURL
+    //       });
+           
+    //     });
+    //   }
+    // )
+  }
+    
+
+  
+  
+
   return (
     <div className="header-container">
       <div className="header-names">
@@ -49,6 +97,8 @@ export const Header = () => {
       <img className="certificate-logo" src={require("./certificate.png")} alt="logo" />
       <p className="certificate-name">Certificate</p>
 
+      <button onClick={uploadImg}>upload</button>
+
       </div>
 
       {/* <div className="header-names">
@@ -58,9 +108,25 @@ export const Header = () => {
       <div>
         <div className={`navigation ${isactive ? "active" : null}`}>
           <div className="userbox">
-            <div className="imgbox">
-              <img src={require("./user.png")} alt="user" />
+
+            <div className="imgbox" >
+
+              {/* <img src={require("./user.png")} alt="user" /> */}
+              <input
+            style={{ display: "none" }}
+            type="file"
+            name=""
+            id="file"
+            required
+            onChange={(e) => setProfilePhoto(e.target.files[0])}
+          />
+          <label htmlFor="file" className="add_avatar">
+            <img src={require("./user.png")} alt="" />
+            <span style={{ color: "white" }}>Add an avatar</span>
+          </label>
+
             </div>
+
             <p className="username">{userName}</p>
           </div>
           <div className="menutoggle" onClick={toggle}>
--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,8 @@ import {getAuth} from "firebase/auth";
 // firestore
 import {getFirestore} from "@firebase/firestore";
 
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAkhnJlRY3nyMPhHftZXXYhrK4xItZJCOY",
@@ -19,5 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default app;
